e664dcca9a8fb61ba2d6bfd30003e0979354ec74
spc <tony_gus25@gmail.com>
2026-09-24 19:13:56 -0500

    Segundo Commit

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,19 +1,3 @@
 from django.contrib import admin
-from .models import Producto
 
-
-@admin.register(Producto)
-class ProductoAdmin(admin.ModelAdmin):
-
-    list_display = (
-        'id',
-        'nombre',
-        'precio',
-        'stock',
-        'categoria'
-    )
-
-    search_fields = (
-        'nombre',
-        'categoria'
-    )
+# Register your models here.
